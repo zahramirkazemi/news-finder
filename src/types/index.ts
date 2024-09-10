33f1ca03d534f86b445139ad0f825f6afb5ba14a
Zahra Mirkazemi <zahramirkazemi1997@gmail.com>
2024-09-10 15:08:38 +0330

    Add types and enum to type-index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,7 @@
 export type { default as Article } from './article';
+export type { default as NewsApiResponse } from './news-api-response';
+export type { default as SearchInType } from './search-in-type';
+export type { default as NewsListFilters } from './news-list-filters';
+export type { default as NewsCategories } from './news-categories';
+export { default as NewsCategoriesEnum } from './news-categories-enum';
+export { default as SearchInEnum } from './search-in-enum';
