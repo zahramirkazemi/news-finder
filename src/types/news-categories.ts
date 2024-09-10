f76b23355215a0a0118acda99b956658bc58e955
Zahra Mirkazemi <zahramirkazemi1997@gmail.com>
2024-09-10 15:01:28 +0330

    Add news-categories type

---
+++ src/types/news-categories.ts
@@ -0,0 +1,5 @@
+import NewsCategoriesEnum from "./news-categories-enum";
+
+type NewsCategories = NewsCategoriesEnum.Business | NewsCategoriesEnum.Entertainment | NewsCategoriesEnum.General | NewsCategoriesEnum.Health | NewsCategoriesEnum.Science | NewsCategoriesEnum.Sports | NewsCategoriesEnum.Technology;
+
+export default NewsCategories;
