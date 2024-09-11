7ac9af84648ecf93839589a1691f47f048f5eec4
Zahra Mirkazemi <zahramirkazemi1997@gmail.com>
2024-09-11 15:47:13 +0330

    Add style to article description

--- src/app/components/articleCard.tsx
+++ src/app/components/articleCard.tsx
@@ -12,7 +12,7 @@ const ArticleCard: React.FC<ArticleCardProps> = ({ article } : ArticleCardProps)
   <section className={styles.container} >
     <h4 className={styles.title}>{article.title}</h4>
     <dl>
-      <dd>{article.description}</dd>
+      <dd className={styles.description}>{article.description}</dd>
     </dl>
     <span className={styles.info}>{`${dateFormat(article.publishedAt)} | ${article.source.name}`}</span>
   </section>
